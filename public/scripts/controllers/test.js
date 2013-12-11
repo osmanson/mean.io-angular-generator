f39f16055c9a12b724fe2d5e555c2200dbf22979
nathan <nathan@nathan-laptop>
2013-12-11 14:22:09 -0600

    Initial Commit

---
+++ public/scripts/controllers/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanApp')
+  .controller('TestCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
